1984d52451549793b29ff7c320265855a366f4d3
Flyn San <flynsarmy@gmail.com>
2022-09-09 17:58:09 +1000

    Back to _ready

--- Main.gd
+++ Main.gd
@@ -4,7 +4,6 @@ const song: AudioStreamSample = preload("res://Stage-1.wav")
 
 onready var player: AudioStreamPlayer = $AudioStreamPlayer
 
-
-func _on_Button_pressed() -> void:
+func _ready() -> void:
 	player.stream = song
 	player.play()
